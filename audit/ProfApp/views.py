56999bea9d721d7c1862f50296bf0812277b976b
Lee-SungMin <saevers26@gmail.com>
2022-05-15 17:32:05 +0900

    fix: url

--- audit/ProfApp/views.py
+++ audit/ProfApp/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'ProfApp/index.html')
+
+def after(request):
+    return render(request, 'ProfApp/class_management_after.html')
