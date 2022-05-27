77d9176ea7638b0da8764e60e573d76f4048a078
jooys130 <wndustn130@gmail.com>
2022-05-28 02:59:02 +0900

    add: create models and migrate in ProfApp

--- audit/ProfApp/models.py
+++ audit/ProfApp/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+        
+class Prof(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        managed = False
+        db_table = 'prof'
+
+
+class Subject(models.Model):
+    id = models.IntegerField(primary_key=True)
+    unit_list = models.CharField(max_length=50)
+
+    class Meta:
+        managed = False
+        db_table = 'subject'
+
+
+class Unit(models.Model):
+    id = models.IntegerField(primary_key=True)
+    apply_period = models.DateTimeField(blank=True, null=True)
+    limit_num = models.IntegerField(blank=True, null=True)
+    audit_type = models.TextField()
+    subject = models.ForeignKey(Subject, models.DO_NOTHING, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'unit'
