00232943f30da769fb167b80bd1dac7789904a64
Winter-gr <aapa2222@naver.com>
2022-06-15 01:51:53 +0900

    add : deducting student mileage and displaying remaining mileage in alert

--- audit/StudApp/views.py
+++ audit/StudApp/views.py
@@ -11,11 +11,11 @@ def reg(request):
     return render(request, 'StudApp/desired_class_registration.html')
 
 def reg_syl(request):
+    hong = Stud.objects.get(id=1234567890)
     if request.method == "POST":
         form = BettingForm(request.POST)
         if form.is_valid():
             mileage = form.cleaned_data['admin_mileage']
-            hong = Stud.objects.get(id=1234567890)
             deducted = hong.mileage - mileage
             hong.mileage = deducted
             hong.save()
@@ -25,14 +25,14 @@ def reg_syl(request):
             Apply.apply_time = timezone.now()
             Apply.student_id = hong.id
             Apply.save()
-            context = {'mileage': mileage}
+            context = {'original': hong.mileage}
             return render(request, 'StudApp/desired_class_registration_syllabus.html', context)
         else :
             return redirect('stud:reg_syl')
 
     else :
         form = BettingForm()
-    context = {'form': form}
+    context = {'form': form, 'original': hong.mileage}
     return render(request, 'StudApp/desired_class_registration_syllabus.html', context)
     # return render(request, 'StudApp/desired_class_registration_syllabus.html')
 
